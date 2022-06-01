67767dbb3b2695c52f2faed2c20d2af5ece1ff35
Eirfire <jacob35422@gmail.com>
2022-06-01 10:37:18 +1000

    code changes

---
+++ assets/Js/nav.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.nav-menu');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('active');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('active');
+    menuOpen = false;
+  }
+});
+
+
+
