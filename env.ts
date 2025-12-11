2d0e7efe9316d56783d5e11bcb38f4f0d93fe068
Jacob <jacob35422@gmail.com>
2025-12-11 21:26:50 +1000

    HOT-FIX: posthog not running

--- env.ts
+++ env.ts
@@ -2,28 +2,24 @@ import { createEnv } from '@t3-oss/env-core'
 import { z } from 'zod'
 
 export const env = createEnv({
-  server: {
-    SERVER_URL: z.string().url().optional(),
-  },
-
-  /**
-   * The prefix that client-side variables must have. This is enforced both at
-   * a type-level and at runtime.
-   */
-  clientPrefix: 'VITE_',
-
-  client: {
-    VITE_APP_TITLE: z.string().min(1).optional(),
-    VITE_POSTHOG_KEY: z.string().optional(),
-    VITE_POSTHOG_HOST: z.url().optional()
-  },
-
   /**
    * What object holds the environment variables at runtime. This is usually
    * `process.env` or `import.meta.env`.
    */
   runtimeEnv: import.meta.env,
-
+  /**
+   * The prefix that client-side variables must have. This is enforced both at
+   * a type-level and at runtime.
+   */
+  clientPrefix: 'VITE_',
+  server: {
+    SERVER_URL: z.string().url().optional(),
+  },
+  client: {
+    VITE_APP_TITLE: z.string().min(1).optional(),
+    VITE_POSTHOG_KEY: z.string().optional(),
+    VITE_POSTHOG_HOST: z.url().optional()
+  },
   /**
    * By default, this library will feed the environment variables directly to
    * the Zod validator.
--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,23 +1,23 @@
-import PosthogProvider from "./posthog";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PosthogProvider from "./posthog";
 
 export function getContext() {
-  const queryClient = new QueryClient();
-  return {
-    queryClient,
-  };
+	const queryClient = new QueryClient();
+	return {
+		queryClient,
+	};
 }
 
 export function Providers({
-  children,
-  queryClient,
+	children,
+	queryClient,
 }: {
-  children: React.ReactNode;
-  queryClient: QueryClient;
+	children: React.ReactNode;
+	queryClient: QueryClient;
 }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <PosthogProvider>{children}</PosthogProvider>
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<PosthogProvider>{children}</PosthogProvider>
+		</QueryClientProvider>
+	);
 }
